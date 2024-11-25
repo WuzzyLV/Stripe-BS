b09058973bbc572760509f679a9e993d91bbfbf3
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-11-25 15:32:38 +0200

    +

--- payments/success.php
+++ payments/success.php
@@ -1,1 +1,32 @@
 <?php
+
+if(!empty($_GET['session_id'])){
+    $session_id = $_GET['session_id'];
+
+    require_once './../stripe-php/init.php';
+    require_once "config.php";
+
+    $statusMsg = '';
+    try{
+
+        $checkout_session = \Stripe\Checkout\Session::retrieve($session_id);
+        $customer_email = $checkout_session->customer_details->email;
+        $payment_intent = \Stripe\PaymentIntent::retrieve($checkout_session->payment_intent);
+
+        if($payment_intent->status === 'succeeded'){
+            $transactionID = $payment_intent->id;
+            $statusMsg = `
+                <h2>Maksajums veikts veiksmigi!</h2>
+                <p>Lai turpmak iegutu pro privilegijas, veicot jaunu pieteikumu, izmantojiet so epastu: <b>${customer_email}</b></p>
+                <p>Maksajuma reference: ${transactionID}</p>
+            `;
+        }else{
+            $statusMsg = "Ej bekot!";
+        }
+ 
+
+
+    }catch(Exception $e){
+        echo "Nevar iegut maksajuma informaciju: " . $e->getMessage();
+    }
+}
